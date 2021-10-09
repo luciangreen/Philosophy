34c22a3752f80f2fefce95f95d8149f2eb88ad22
Lucian Green <luciangreen@lucianacademy.com>
2021-10-10 00:41:44 +1100

    Update lfl2html.pl

--- lfl2html.pl
+++ lfl2html.pl
@@ -5,23 +5,39 @@
 % [[b],"Hello"] -> <b>Hello</b>
 % [[p],[[b],"Hello"]] -> <p><b>Hello</b></p>
 
+/*
+
+[[html],[[head],[[title],"Title"]],[[body],[[p],[[b],"Hello"]]]]
+
+H = "<html><head><title>Title</title></head><body><p><b>Hello</b></p></body></html>".
+
+*/
+
 lfl2html(HTML) :-
 	read_string(user_input,'\n','\r',_,Input),
 	term_to_atom(Term,Input),
 	lfl2html2(Term,HTML),!.
 	
 lfl2html2([],[]).
+
+lfl2html2(String,String) :- string(String),!.
+	
+lfl2html2(Term,HTML) :-
+	lfl2html_codes(Tag,Open_tag,Close_tag),
+	(Term=[Tag|String]->
+	(lfl2html2(String,HTML3),
+	(is_list(HTML3)->concat_list(HTML3,HTML2);HTML2=HTML3),
+	%trace,
+	concat_list([Open_tag,HTML2,Close_tag],HTML))).
+
+
 lfl2html2([Term|Term2],[HTML|HTML2]) :-
 	lfl2html2(Term,HTML),
 	lfl2html2(Term2,HTML2).
 	
-lfl2html2(Term,HTML) :-
-	(Term=[[p],String]->
-	(lfl2html2(String,HTML2),
-	concat_list(["<p>",HTML2,"</p>"],HTML))).
-
-lfl2html2(Term,HTML) :-
-	(Term=[[b],String]->
-	concat_list(["<b>",String,"</b>"],HTML)).
-	
-	
+lfl2html_codes([html],"<html>","</html>").
+lfl2html_codes([head],"<head>","</head>").
+lfl2html_codes([body],"<body>","</body>").
+lfl2html_codes([title],"<title>","</title>").
+lfl2html_codes([p],"<p>","</p>").
+lfl2html_codes([b],"<b>","</b>").
