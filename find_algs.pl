57f6743021e4677217072c2ccecbcb0e9f2e9938
Lucian Green <luciangreen@lucianacademy.com>
2021-10-12 03:26:49 +1100

    Find details 3 and Find algs

---
+++ find_algs.pl
@@ -0,0 +1,125 @@
+% find_algs.pl - finds sequence of n algs with types x also lpiv, (later: lg on gh) through words in descriptions x, replaces words in alg with a word from the description to make them link together
+% - from randomly selected algs x later
+
+:-include('../listprologinterpreter/listprolog.pl').
+
+find_algs(T4) :-
+
+		phrase_from_file_s(string(String00a), "file_fa.txt"),
+		string_codes(String02b,String00a),
+
+ 	SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!0123456789", % doesn't have "'" xx
+
+	split_string(String02b, ".\n\r", ".\n\r", String02cb),
+
+	findall([String02cd,H],(member(H,String02cb),
+		split_string(H, SepandPad,SepandPad, String02cd)),
+	Texts3),
+
+	findall([_A,B,C,D],test(24,B,C,D),E),
+	%findall([A,B,C,D],test_types_cases(A,B,_,_,C,D),F),
+	F=[],
+	append(E,F,G),
+%trace,
+	findall([T2,QueryB,C3,D3],(member(T3,Texts3),
+	find_first_working_alg(T3,G,[T2,QueryB,C3,D3])),
+	T4).
+
+find_first_working_alg(T3,G,[T2,QueryB,C3,D3]) :-
+	T3=[T1,T2],member(A1,T1),member(A2,T1),
+	not(A1=A2),
+	
+	random_member([_A,B,C,D],G),
+	%trace,
+	B=[[n,N],Args1],
+	
+	%trace,
+	member(B1,Args1),not(B1=[v,_]),
+	[D1]=D,
+	member([[v, _], B2],D1),%not(B2=[v,_]),
+	
+	replace_a_word(A1,B1,Args1,[],Args2),
+	replace_a_word(A2,B2,Args2,[],Args3),
+	
+	QueryB=[[n,N],Args3],
+	%trace,
+	replace_a_word(A1,B1,C,[],C2), % it doesn't replace variable names
+	replace_a_word(A2,B2,C2,[],C3),
+
+	replace_a_word(A1,B1,D1,[],D2),
+	replace_a_word(A2,B2,D2,[],D4),
+	trace,
+	[D4]=D3,
+	%trace,
+	%writeln1(interpret(off,QueryB,C3,D3)),
+	(interpret(off,QueryB,C3,D3)->true;
+	find_first_working_alg(T3,G,[T2,QueryB,C3,D3])),!.
+
+
+	
+	%delete(Args10,[v,_],Args1),
+
+
+replace_a_word(_A1,_B1,[],Args,Args).
+replace_a_word(A1,B1,Args1,Args2,Args3) :-
+	Args1=[A222|A3],
+	((is_list(A222),not(variable_name(A222)))->
+	(replace_a_word(A1,B1,A222,[],A21),
+	A21=A2);%append(Args2,A21,A2));
+	A222=A2),
+%writeln1([a2,A2]),
+	(((A2=[v,_V]->true;not(A2=B1)))->
+	(append(Args2,[A2],Args4),
+	replace_a_word(A1,B1,A3,Args4,Args3));
+	
+	(
+	term_to_atom(A2,A21),
+	string_codes(A21,Args21),
+	string_codes("[]\"'",Codes1),
+	split_on_substring117(Args21,Codes1,[],List),
+
+%trace,
+	replace_a_word1(A1,List,[],List3),
+	%term_to_atom(List21,List22),
+	%string_atom(List3,List22),
+	%writeln1(List21),
+	%trace,
+	%maplist(append,[[List21]],[List2]),
+	%maplist(append,[List22],[List23]),
+	%maplist(append,[List23],[List2]),
+	%concat_list(List2,List3),
+	%maplist(append,[[Args2,List3,A3]],[Args3]))).
+	%trace,
+	maplist(append,[[List3]],[List31]),
+
+	%(false%maplist(append,[[Args2,List31]],[[Args52]])
+	%->true;
+	maplist(append,[[Args2,List31]],[Args52]),%),
+	%((trace,
+	%is_list(Args52),Args52=[Args53],is_list(Args53))->
+	%Args51=Args53;Args51=Args52),
+	%concat_list(Args51,Args5),
+	%findall(E2,(member(E1,Args52),
+	%(is_list(E1)->(concat_list(E1,E3),[E3]=E2);E1=E2)),Args5),
+	%term_to_atom(Args52,Args53),
+	%string_atom(Args5,Args53),
+	replace_a_word(A1,B1,A3,Args52,Args3))).
+	
+replace_a_word1(_A1,[],List,List).
+replace_a_word1(A1,List1,List2,List3) :-
+	List1=[L|Ls],
+	%trace,
+	(((L="["->true;(L="]"->true;(L="\""->true;(L="'"))))->
+	((L="\""->L1="";L=L1),append(List2,[L1],List41),
+	concat_list(List41,List4),
+	replace_a_word1(A1,Ls,[List4],List3)));
+	%maplist(append,[[List2,A1,Ls]],[List3])).
+	(%trace,
+	maplist(append,[[List2,[A1]]],List52),
+	maplist(append,[List52],[List51]),
+	concat_list(List51,List5),
+	replace_a_word1(A1,Ls,[List5],List3))).
+
+	
+	
+
---
+++ find_details3.pl
@@ -0,0 +1,124 @@
+% find_details3.pl
+
+% find_db - converts texts into lowercase, splits, removes non trivial words (keeps original sentences as well)
+
+% find_details3 - finds conns through sentences, ()removes duplicates without sorting
+% - from randomly selected texts x later
+
+%:-include('../Lucian-Academy/folders.pl').
+:-include('folders1.pl').
+:-include('../listprologinterpreter/la_files.pl').
+:-include('../listprologinterpreter/la_strings.pl').
+:-include('../listprologinterpreter/la_strings_string.pl').
+
+find_db :-
+
+	folders(Folders),
+	
+	findall(Texts11,(member(Dept,Folders), % Dept2->Dept
+	%concat_list(["../Lucian-Academy/",Dept2,"/"],Dept),
+	concat_list([Dept,"/"],Dept1),
+	directory_files(Dept1,F),
+	delete_invisibles_etc(F,G),
+ 	SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!0123456789", % doesn't have "'" xx
+
+	findall(Texts3,(member(Filex1,G),
+	string_concat(Dept1,Filex1,Filex),
+		phrase_from_file_s(string(String00a), Filex),
+		string_codes(String02b,String00a),
+		
+		%downcase_atom(String02b,String00a1),
+		String02b=String00a1,
+		
+	split_string(String00a1, ".\n\r", ".\n\r", String02cb),
+
+	findall([String02cd,H],(member(H,String02cb),
+	downcase_atom(H,H1),
+	split_string(H1, SepandPad,SepandPad, String02cd)),
+	Texts3)
+	%maplist(append,[Texts3],[Texts31])
+		
+		),Texts1),
+			maplist(append,[Texts1],[Texts11])
+),
+		Texts21),
+				maplist(append,[Texts21],[Texts2]),
+
+% a b
+
+ 	phrase_from_file_s(string(Codes_cve), "connectives.txt"),
+	string_codes(String_cve,Codes_cve),
+	atom_to_term(String_cve,Connectives,_),
+
+	findall([A,WC],(member([B,WC],Texts2),subtract(B,Connectives,A)),C),
+	
+	term_to_atom(C,D82),
+	string_atom(D83,D82),
+
+	(open_s("find_details3_db.txt",write,Stream1),
+	write(Stream1,D83),
+	close(Stream1)),!.
+
+
+find_details3 :-
+
+		phrase_from_file_s(string(String00a), "find_details3_db.txt"),
+		string_codes(String02b,String00a),
+		atom_to_term(String02b,DB,[]),
+
+		phrase_from_file_s(string(String00a1), "file_fd3.txt"),
+		string_codes(String02b1,String00a1),
+		downcase_atom(String02b1,String02b12),
+		
+ 	SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!0123456789", % doesn't have "'" xx
+
+	split_string(String02b12, ".\n\r", ".\n\r", String02cb),
+
+	findall([String02cd,H],(member(H,String02cb),
+		split_string(H, SepandPad,SepandPad, String02cd)),
+	Texts3),
+
+findall([Sent,Chain4],
+(member([B,Sent]
+,Texts3),
+
+(member(C,B),member(D,B),
+not(C=D),
+
+dfs1(C,D,Sent,DB,_,0,[],Chain4))),T1),
+
+T1=[T|_],
+
+writeln1(T).
+
+dfs1(C51,D51,Sent,DB,_,A,[],Chain2) :-
+ dfs(C51,D51,Sent,DB,_,A,[],Chain2),!.
+
+max_depth(3).
+
+
+dfs(C1,D1,Sent,DB,DB2,N,Chain1,Chain2) :-
+	max_depth(N0),
+	N<N0,
+	((member([E3,E],DB),
+	member(C1,E3),	member(D1,E3),
+	not(member(E,Chain1)),
+	append(Chain1,[E],
+	Chain2),
+	DB=DB2)->true;
+	(member([E3,E],DB),
+	findall([C22,E],(
+	member(C1,E3),
+	member(C22,E3),not(C1=C22)),C2),
+	delete(DB,[E3,E],DB3), % unlike find db 1, doesn't find details from same sentence again
+	(C2=[]->fail;
+	(member([C3,C32],C2),%|C4],
+	N1 is N+1,
+	not(member(C32,Chain1)),
+
+	append(Chain1,[
+	C32],Chain3),
+	dfs(C3,D1,Sent,DB3,DB2,N1,Chain3,Chain2))))).
+
+
+	
