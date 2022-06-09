9796030090190b3825efa7cda71f9722dca7320d
Lucian Green <luciangreen@lucianacademy.com>
2022-06-09 14:56:12 +1000

    Corrected bugs in types converter

--- curly_brackets.pl
+++ curly_brackets.pl
@@ -4,9 +4,14 @@ t_or_empty({[t,_]}).
 t_or_empty([t,_]).
 t_or_empty([]).
 
+/*
 %curly_head_tail(T,List1,List2) :- t_or_empty(T),append(List1,T,List2),!.
 curly_head_tail({T},{H},Ta) :- curly_head_tail(T,H,Ta),%,append(List1,T,List2),
 !.
+%curly_head_tail({T},{H},Ta) :- 
+% T =.. [{}, Round_bracket_list],
+%curly_head_tail(T,H,Ta),%,append(List1,T,List2),
+%!.
 
 curly_head_tail(Head1,C1,C2) :-
 
@@ -21,12 +26,12 @@ curly_head_tail(Head1,C1,C2) :-
  	
  %findall(A,(member(N1,N),arg(N1,Round_bracket_list,A)),[Head1a|Head1b]).
 
-/*
+*
 curly_head_tail1(Round_bracket_list,List1,List2) :-
 
  Round_bracket_list =.. [t,A],
  append(List1,[[t,A]],List2),!.
-*/
+*
 
 curly_head_tail1(T,List1,List2) :- t_or_empty(T),append(List1,[T],List2),!.
 
@@ -45,6 +50,44 @@ curly_head_tail1(Round_bracket_list,List1,List2) :-
  append(List1,[T],List3),
  curly_head_tail1(B,List3,List2),!.
 
+*/
+%/*
+
+%curly_head_taila(T,H,Ta),%,append(List1,T,List2),
+%!.
+
+curly_head_taila(Head1,C1,C2) :-
+
+ Head1 =.. [{}, Round_bracket_list],
+ 
+ curly_head_tail1a(Round_bracket_list,[],[Head1a|Head1b]),
+ 
+ (Round_bracket_list =.. [{}, _]-> (square_to_curly(Head1a,C1),(
+ %(Head1b =.. [{}, _]-> square_to_curly(Head1b,C2);
+ Head1b=C2))%)
+ ;
+ [Head1a|Head1b]=[C1|C2]),!.
+
+
+curly_head_tail1a(T,List1,List2) :- t_or_empty(T),append(List1,[T],List2),!.
+
+%curly_head_tail1([T],List1,List2) :- t_or_empty(T),append(List1,%[[T]],List2),!.
+
+curly_head_tail1a(Round_bracket_list,List1,List2) :-
+ curly_head_taila(Round_bracket_list,List3,List4),
+ append(List1,[[List3|List4]],List2).
+
+curly_head_tail1a(Round_bracket_list,List1,List2) :-
+ 
+ %t_or_empty(T),
+
+ Round_bracket_list =.. [',', T,  B],
+ %curly_head_tail(T,_,_),
+ append(List1,[T],List3),
+ (B=..[{}, _]->append(List3,[B],List2);
+ curly_head_tail1a(B,List3,List2)),!.
+%*/
+ 
  
 square_to_curly(A,B) :-
  square_to_round(A,C),round_to_curly(C,B).
@@ -56,7 +99,7 @@ round_to_curly(A,B) :-
 % A = (1, 2, 3).
 
 square_to_round(A,C) :-
- t_or_empty(T),
+ %t_or_empty(T),
  ((A=[D]%,D=T
  )->
  C=(D);
--- expand_types.pl
+++ expand_types.pl
@@ -62,6 +62,7 @@ get_lang_word("string",Dbw_string),
 
 Data=[T,Dbw_string],	%string(Data),
 append(Types1,[[T,Dbw_string]],Types2),!.
+/*
 expand_types1(Data1,Types1,Types2) :-
 get_lang_word("t",T),
 get_lang_word("brackets",Dbw_brackets),
@@ -72,7 +73,7 @@ get_lang_word("brackets",Dbw_brackets),
 	expand_types1(Data2,[],Types4),
 	Types5=[[[T,Dbw_brackets],Types4]],
 	append_list([Types1,Types5],Types2),!.
-
+*/
 /*
 expand_types1(Data1,Types1,Types2) :-
 get_lang_word("t",T),
@@ -91,32 +92,14 @@ get_lang_word("brackets",Dbw_brackets),
 	Data1=[[Data2|Data3]|Data41],
 
 not([Data2|Data3]=[T,_]),
-	expand_types1(Data2,[],Types3),
+	expand_types1([Data2],[],Types3),
 	expand_types1(Data3,Types3,Types4),
 	Types5=[[[T,Dbw_brackets],Types4]],
 
-findall(Types61,(member(Data4,Data41),	expand_types1(Data4,[],Types61)),Types62),
+findall(Types61,(member(Data4,Data41),	expand_types1([Data4],[],Types61)),Types62),
 foldr(append,Types62,Types6),
 	append_list([Types1,Types5,Types6],Types2),!.
 
-expand_types1(Data1,Types1,Types2) :-
-get_lang_word("t",T),
-get_lang_word("list",Dbw_list),
-	Data1=[Data2Data3|Data41],
-
-not(Data2Data3=[T,_]),
-
-curly_head_tail(Data2Data3,Data2,Data3),
-	expand_types1(Data2,[],Types3),
-	expand_types1(Data3,Types3,Types4),
-	Types5=[[[T,Dbw_list],Types4]],
-%trace,
-findall(Types61,(member(Data4,Data41),	expand_types1(Data4,[],Types61)),Types62),
-foldr(append,Types62,Types6),
-	append_list([Types1,Types5,
-	Types6],Types2),!.
-	
-	
 	expand_types1(Data1,Types1,Types2) :-
 get_lang_word("t",T),
 get_lang_word("list",Dbw_list),
@@ -124,8 +107,9 @@ get_lang_word("list",Dbw_list),
 
 %not(Data2Data3=[T,_]),
 not(Data1=[T,_]),
-
-curly_head_tail(Data1,Data2,Data3),
+%trace,
+curly_head_taila(Data1,Data2,Data3),
+%trace,
 	expand_types1(Data2,[],Types3),
 	expand_types1(Data3,Types3,Types4),
 	Types5=[[[T,Dbw_list],Types4]],
@@ -135,6 +119,26 @@ curly_head_tail(Data1,Data2,Data3),
 %foldr(append,Types62,Types6),
 	append_list([Types1,%Types5,
 	Types5],Types2),!.
+
+
+expand_types1(Data1,Types1,Types2) :-
+get_lang_word("t",T),
+get_lang_word("list",Dbw_list),
+	Data1=[Data2Data3|Data41],
+
+not(Data2Data3=[T,_]),
+
+curly_head_taila(Data2Data3,Data2,Data3),
+	expand_types1(Data2,[],Types3),
+	expand_types1(Data3,Types3,Types4),
+	Types5=[[[T,Dbw_list],Types4]],
+%trace,
+findall(Types61,(member(Data4,Data41),	expand_types1([Data4],[],Types61)),Types62),
+foldr(append,Types62,Types6),
+	append_list([Types1,Types5,
+	Types6],Types2),!.
+	
+	
 	expand_types1(Data,Types1,Types2) :-
 get_lang_word("t",T),
 %get_lang_word("string",Dbw_string),
