6949f696b64986126c609e03efe42d97ebbfddd9
Lucian Green <luciangreen@lucianacademy.com>
2022-07-15 16:22:22 +1000

    Fixed bug with [a,[]]

--- list_to_string.pl
+++ list_to_string.pl
@@ -2,11 +2,14 @@
 
 list_to_string(A,B) :-
  list_to_string(A,"",B),!.
+ 
 list_to_string([],B,C) :-
  foldr(string_concat,["[",B,"]"],C),!.
 	
 list_to_string(A,B,C) :-
  (not(is_list(A))->((B=""->G="";G=","),
  foldr(string_concat,[B,G,A],C));
- (A=[D|E],list_to_string(D,B,F),
- list_to_string(E,F,C))),!.
+ (A=[D|E],list_to_string(D,"",F),
+ (B=""->G="";G=","),
+ foldr(string_concat,[B,G,F],F1),
+ list_to_string(E,F1,C))),!.
