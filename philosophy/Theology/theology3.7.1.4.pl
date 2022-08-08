1167a143e32aff05844c735d3b77679d2a238958
Lucian Green <luciangreen@lucianacademy.com>
2022-08-09 00:25:29 +1000

    Update theology3.7.1.4.pl

--- philosophy/Theology/theology3.7.1.4.pl
+++ philosophy/Theology/theology3.7.1.4.pl
@@ -1,10 +1,4 @@
-rainforest(
 /*
-[[1,9,["*"]],[2,9,["*"]],[3,9,["*"]],[4,9,["*"]],[5,9,["*"]],[6,9,["*"]],
-[1,8,["*"]],[2,8,[]],[3,8,["m"]],[4,8,["vine"]],[5,8,[]],[6,8,["*"]],
-[1,7,["*"]],[2,7,["*"]],[3,7,["*"]],[4,7,["*"]],[5,7,["*"]],[6,7,["*"]]]).
-*/
- %/*
 [[1,9,[     ]],[2,9,[     ]],[3,9,["*"  ]],[4,9,["*"  ]],[5,9,["*"  ]],
  [1,8,[     ]],[2,8,["*"  ]],[3,8,[     ]],[4,8,[     ]],[5,8,["*"  ]],
  [1,7,[     ]],[2,7,["*"  ]],[3,7,[     ]],[4,7,["*"  ]],[5,7,[     ]],
@@ -40,53 +34,16 @@ Game Over
 
 **/
 
-traverse(X,Y) :-
-	traverse(X,Y,[],_,[],_,"n",_),!.
-
+rainforest([[1,9,["*"]],[2,9,["*"]],[3,9,["*"]],[4,9,["*"]],[5,9,["*"]],[6,9,["*"]],[7,9,["*"]],[1,8,["*"]],[2,8,["k"]],[3,8,["c"]],[4,8,["vine"]],[5,8,[]],[6,8,[]],[7,8,["*"]],[1,7,["*"]],[2,7,["*"]],[3,7,["*"]],[4,7,["*"]],[5,7,["*"]],[6,7,["*"]],[7,7,["*"]]]).
+traverse(X,Y):-(traverse(X,Y,[],_,[],_,"n",_)->(true);(false)),!.
 traverse(_X,_Y,Explored,Explored,Inventory,Inventory,E,E) :-
  (E="e"->!;fail),!.
-	%traverse(_X,_Y,Explored,Explored,Inventory,Inventory,"n",_) :-!.
-
-traverse(X,Y,Explored,Explored,Inventory,Inventory,E,E) :-
-	rainforest(Map),
-	member([X,Y,Cell],Map),
-	Cell=["*"],!.
-
- 
-traverse(X,Y,Explored,Explored,Inventory,Inventory,E,E) :-
-	member([X,Y],Explored),!.
-traverse(X,Y,Explored1,Explored2,Inventory1,Inventory2,E1,E2) :-
-	rainforest(Map),
-	member([X,Y,Cell],Map),
-	write([X,Y]),
-	(Cell=[]->Inventory4a=Inventory1;
-		(Cell=[Item],append(Inventory1,[Item],Inventory3),
-		apply_all_to_all(Inventory3,Inventory4),Inventory4a=Inventory4)),
-	writeln(Inventory4a),
-	(member("e",Inventory4a)->(writeln("Game Over"),Inventory4a=Inventory2,Explored1=Explored2,
-	E2="e"%
-	%,!
-	%,trace
-	);(%E1=E2,
-	append(Explored1,[[X,Y]],Explored3),
-	Xm1 is X-1,
-	Ym1 is Y-1,
-	Xp1 is X+1,
-	Yp1 is Y+1,
-	traverse(Xm1,Y,Explored3,Explored4,Inventory4a,Inventory5,E1,E3),
-	traverse(X,Ym1,Explored4,Explored5,Inventory5,Inventory6,E3,E4),
-	traverse(Xp1,Y,Explored5,Explored6,Inventory6,Inventory7,E4,E5),
-	traverse(X,Yp1,Explored6,Explored2,Inventory7,Inventory2,E5,E2))).
-
-apply_all_to_all(Inventory1,Inventory2) :-
-	findall(Item3,(member(Item1,Inventory1),
-	member(Item2,Inventory1),not(Item1=Item2),
-	apply(Item1,Item2,Item3),
-	not(member(Item3,Inventory1))),
-	AddedItems),
-	(AddedItems=[]->Inventory1=Inventory2;
-	(append(Inventory1,AddedItems,Inventory3),
-	apply_all_to_all(Inventory3,Inventory2))).
-
+traverse(X,Y,Explored1,Explored2,Inventory1,Inventory2,E1,E2):-rainforest(Map),member2(Map,[X,Y,Cell]),((equals4(Cell,["*"])->(true);(member2(Explored1,[X,Y])))->((equals4(Explored1,Explored2),equals4(Inventory1,Inventory2),E2=E1));((writeln([X,Y]),(equals4(Cell,[])->(equals4(Inventory4a,Inventory1));((equals4(Cell,[Item]),append(Inventory1,[Item],Inventory3),apply_all_to_all(Inventory3,Inventory4),equals4(Inventory4a,Inventory4)))),writeln(Inventory4a),(member2(Inventory4a,"e")->((writeln("Game Over"),equals4(Explored1,Explored2),equals4(Inventory2,Inventory4a),E2="e"));((append(Explored1,[[X,Y]],Explored3),-(X,1,Xm1),-(Y,1,Ym1),+(X,1,Xp1),+(Y,1,Yp1),traverse(Xm1,Y,Explored3,Explored4,Inventory4a,Inventory5,E1,E3),traverse(X,Ym1,Explored4,Explored5,Inventory5,Inventory6,E3,E4),traverse(Xp1,Y,Explored5,Explored6,Inventory6,Inventory7,E4,E5),traverse(X,Yp1,Explored6,Explored2,Inventory7,Inventory2,E5,E2))))))).
+apply_all_to_all(Inventory1,Inventory2):-findall(Item3,(member2(Inventory1,Item1),member2(Inventory1,Item2),not((equals4(Item1,Item2))),apply(Item1,Item2,Item3),not((member2(Inventory1,Item3)))),Addeditems),(equals4(Addeditems,[])->(equals4(Inventory1,Inventory2));((append(Inventory1,Addeditems,Inventory3),apply_all_to_all(Inventory3,Inventory2)))).
 apply("k","c","m").
 apply("m","vine","e").
+
+member2(A,B):-member(B,A).
+equals4(A,B):-A=B.
++(A,B,C):-C is A+B.
+-(A,B,C):-C is A-B.
