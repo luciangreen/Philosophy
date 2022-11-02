d069a680d6c8789904da0e43dbaadfd66415defa
Lucian Green <luciangreen@lucianacademy.com>
2022-11-02 23:38:44 +1100

    Update string_concat1.pl

--- string_concat1.pl
+++ string_concat1.pl
@@ -1,8 +1,8 @@
-string_concat1(A,B,C):-string_chars1(A,A1),string_chars1(B,B1),string_chars1(C,C1),string_concat2(A1,B1,C1),string_chars1(A,A1),string_chars1(B,B1),string_chars1(C,C1).
+string_concat1(A,B,C):-string_chars1(A,A1),string_chars1(B,B1),string_chars1(C,C1),append1(A1,B1,C1),string_chars1(A,A1),string_chars1(B,B1),string_chars1(C,C1).
 string_chars1(A,B):-var(A),var(B),!.
 %string_chars1(A,B):-var(B),!.
 string_chars1(A,B):-%not(var(A)),not(var(B)),
  string_chars(A,B).
 %string_chars1(A,B):-not(var(B)),string_chars(A,B).
-string_concat2([],A,A).
-string_concat2([A|D],B,[A|C]) :- string_concat2(D,B,C).
+append1([],A,A).
+append1([A|D],B,[A|C]) :- append1(D,B,C).
