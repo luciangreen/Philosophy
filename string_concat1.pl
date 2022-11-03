f731ff5ad0381e71e8de31f89b496f36710859ec
Lucian Green <luciangreen@lucianacademy.com>
2022-11-03 21:43:50 +1100

    Update string_concat1.pl

--- string_concat1.pl
+++ string_concat1.pl
@@ -1,3 +1,15 @@
+/*
+                                                            string_concat1(A,"","ab").
+A = "ab" ;
+                                                             string_concat1("a","b",C).
+C = "ab".
+                                                              string_concat1(A,"b","ab").
+A = "a" ;
+                                                              string_concat1("a",B,"ab").
+B = "b".
+
+*/
+
 string_concat1(A,B,C):-((var(A),var(C))->fail;(string_chars1(A,A1),string_chars1(B,B1),string_chars1(C,C1),append1(A1,B1,C1),string_chars1(A,A1),string_chars1(B,B1),string_chars1(C,C1))).
 string_chars1(A,B):-var(A),var(B),!.
 %string_chars1(A,B):-var(B),!.
