948c1673562b97f11e640b1a7ac5487f1cb679bd
Lucian Green <luciangreen@lucianacademy.com>
2022-11-06 15:50:17 +1100

    intersection and subtract

---
+++ subtract1.pl
@@ -0,0 +1,7 @@
+%subtract1(A,B,C):-subtract1(A,B,[],C).
+subtract1(C,[],C):-!.
+subtract1(A,[B1|B2],E):-delete1(A,B1,[],D),subtract1(D,B2,E).
+
+delete1([],_,C,C):-!.
+delete1([A1|A2],A1,C,D):-delete1(A2,A1,C,D),!.
+delete1([A1|A2],B,C,D):-delete1(A2,B,[A1|C],D),!.
