68f212d4e7d085179370f92e8949711e06e98bb9
Lucian Green <luciangreen@lucianacademy.com>
2023-02-06 00:24:23 +1100

    Update find_lists.pl

--- find_lists.pl
+++ find_lists.pl
@@ -53,22 +53,40 @@ find_lists1(T1,T2,L1,L2) :-
  find_lists(T1,T2,L1,L2,true).
  
 find_lists([],[],L,L,_) :- !.%maplist(is_empty_list,Ts),!.
+find_lists(_,[],L,L,_) :- !.%,fail.%maplist(is_empty_list,Ts),!.
 find_lists(T,T,L1,L2,_Start) :-
  number(T),
  %trace,
- append(L1,T,L2).
+ append(L1,T,L2),!.
 find_lists(T1,T2,L1,L2,_Start) :-
  length(T1,TL1),
  length(T2,TL2),
  TL1 is TL2*2,
  length(A,TL2),
  append(A,B,T1),
+ %trace,
  check_same(A,B,L1,L2,true),
- check_same(A,T2,L1,_,true).
+ %trace,
+ % * find 2 sides and directions that work
+ (check_same(A,T2,L1,L2,true)->true;
+ check_same(T2,B,L1,L2,true)),!.
+ /*
+find_lists(T1,T2,L1,L2,_Start) :-
+ length(T1,TL1),
+ length(T2,TL2),
+ TL2 is TL1*2,
+ length(A,TL1),
+ append(A,B,T2),
+ %trace,
+ check_same(A,B,L1,L2,true),
+ %trace,
+ check_same(A,T1,L1,L2,true),!.
+ */
 find_lists(T1,T2,L1,L2,_Start) :-
  length(T1,TL),
  length(T2,TL),
- check_same(T1,T2,L1,L2,true).
+ check_same(T1,T2,L1,L2,true),!.
+
  
 check_same([],[],L,L,_) :- !.
 check_same(A,B,L1,L2,Start) :-
