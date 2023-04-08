d8ab640c39d613b3d0317e7df5e2f70d1f827207
Lucian Green <luciangreen@lucianacademy.com>
2023-04-08 20:22:30 +1000

    Update 8 4 23.pl

--- 8 4 23.pl
+++ 8 4 23.pl
@@ -4,6 +4,8 @@
 
 :-include('big_connections_with_bag3_and_mr_short_books-rel.pl').
 
+% How can the program cope with real variation? 2 of 4
+
 % 11.    I prepared to help the girl onto the tram stop.  I did this by writing that the feminine gender of the noun was given by the example, 'girl'.  First, I noted that the person was female.  Second, I noted that the person was young.  Third, I called her a girl.  In this way, I prepared to help the girl onto the tram stop by writing that the feminine gender of the noun was given by the example, 'girl'.
 
 % find key sents with a key term, relate other sents in para in chain of terms
