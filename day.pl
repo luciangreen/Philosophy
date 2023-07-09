5c0c56cbff0d8417cb50fa66d23fc59ce13b1295
Lucian Green <luciangreen@lucianacademy.com>
2023-07-09 11:09:26 +1000

    Update day.pl

--- day.pl
+++ day.pl
@@ -17,14 +17,22 @@ TSD is TS01-TS0,
 get_time(TS1),
 In_a_day is TS1+TSD,
 
-day_loop(In_a_day),
+
+Hours=[9,12,15],
+
+get_time(TS),stamp_date_time(TS,date(Year,Month,Day,Hour1,Minute1,Seconda,_A,_TZ,_False),local),
+
+findall(TSA0,(member(H,Hours),date_time_stamp(date(Year,Month,Day,H,0,0,_,_,_),TSA0)),TSAA),
+
+
+day_loop(0,In_a_day,TSAA),
 
  	nl,
     halt.
 main :- halt(1).
 
 
-day_loop(In_a_day) :-
+day_loop(NB,In_a_day,TSA) :-
 
 get_time(TS1),
 (TS1>In_a_day->true;
@@ -35,12 +43,24 @@ foldr(string_concat,["./day.sh"],S3)%,
 	fail%abort
  	)),
 
+%get_time(TS1),
+NB1 is NB+6000,
+
+findall(TS2,(member(TS2,TSA),TS1>TS2,
+
+foldr(atom_concat,['echo "" | mutt -s "Alarm ',NB1,' Breasonings" luciangreen@lucianacademy.com',''],A1),
+
+shell1(A1)
+
+),TS3),
+subtract(TSA,TS3),
+
 
 %atom_concat('echo "" | mutt -s "BAG Day Test" luciangreen@lucianacademy.com','',A1),
 
 %shell1(A1)
 
- 	day_loop(In_a_day)
+ 	day_loop(NB1,In_a_day,TS3)
  	
  	)),!.
 
