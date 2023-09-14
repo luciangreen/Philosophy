39bf8bc3f1ef11a6094fe26cf9699ca7e6f3580a
Lucian Green <luciangreen@lucianacademy.com>
2023-09-15 09:46:30 +1000

    Added auto mode to paraphraser

--- paraphraser1.pl
+++ paraphraser1.pl
@@ -12,15 +12,28 @@ swipl
 paraphraser.
 */
 
+:-dynamic auto/1.
+
 :- include('../listprologinterpreter/listprolog').
 
+paraphraser(Parameters,File_list_a,Auto) :-
+	retractall(auto(_)),
+	assertz(auto(Auto)),
+	paraphraser(Parameters,File_list_a),!.
+
 paraphraser([string,String],File_list_a) :-
+	(not(catch(auto(_),_,false))->
+	(retractall(auto(_)),
+	assertz(auto(off)));true),
 	%File1="test1.pl",
 	string_codes(String,Codes),
 	paraphraser1(Codes,File_list_a),!.
 	
 paraphraser([file,File1],File_list_a
 ) :-
+	(not(catch(auto(_),_,false))->
+	(retractall(auto(_)),
+	assertz(auto(off)));true),
 	%File1="test1.pl",
 	phrase_from_file_s(string(Codes), File1),
 	paraphraser1(Codes,File_list_a),
@@ -82,13 +95,21 @@ paraphrase1(File_list,File_list1,File_list2,Synonym_list,Synonym_list2) :-
 	%letters(File_list5),
 	string_codes(File_list5,File_list5_c),
 	phrase(word1(File_list5_c),_),
-	((member([File_list3,Synonym],Synonym_list)->true;
+	(((member([File_list3,Synonym],Synonym_list)->true;
 	member([Synonym,File_list3],Synonym_list))->
 	(append(File_list1,[Synonym],File_list6),
 	Synonym_list=Synonym_list1);
+	((%true%trace,
+	%trace,
+	auto(off))
+	->
 	(concat_list(["What is a synonym for: ",File_list3,"\n","or <Return> to skip."],Notification),writeln(Notification),
 	read_string(user_input, "\n", "\r", _End, Input),
-	(Input=""->(Synonym2=File_list3);(Synonym2=Input)),append(Synonym_list,[[File_list3,Synonym2]],Synonym_list1)),
+	(Input=""->(Synonym2=File_list3);(Synonym2=Input)),append(Synonym_list,[[File_list3,Synonym2]],Synonym_list1));
+	(%File_list1=File_list6,
+	Synonym2=File_list3,
+	Synonym_list=Synonym_list1)
+	)),
 	append(File_list1,[Synonym2],File_list6)),
 	paraphrase1(File_list4,File_list6,File_list2,Synonym_list1,Synonym_list2).
 
