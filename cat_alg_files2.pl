f0b202aee83555fec6681792354b4c3b0500eded
Lucian Green <luciangreen@lucianacademy.com>
2024-04-15 09:17:57 +1000

    Bug fix

--- cat_alg_files2.pl
+++ cat_alg_files2.pl
@@ -1,6 +1,6 @@
 :-include('../listprologinterpreter/listprolog.pl').
-main:-catch(shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_alg_files -c cat_alg_files.pl
-./cat_alg_files"),Err,handle_error(Err)),halt.
+main:-catch((shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_alg_files -c cat_alg_files.pl"),
+shell1_s("./cat_alg_files")),Err,handle_error(Err)),halt.
 
 handle_error(_Err):-
   halt(1).
--- cat_arg_files2.pl
+++ cat_arg_files2.pl
@@ -1,6 +1,6 @@
 :-include('../listprologinterpreter/listprolog.pl').
-main:-catch(shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_arg_files -c cat_arg_files1.pl
-./cat_arg_files"),Err,handle_error(Err)),halt.
+main:-catch((shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_arg_files -c cat_arg_files1.pl"),
+shell1_s("./cat_arg_files")),Err,handle_error(Err)),halt.
 
 handle_error(_Err):-
   halt(1).
