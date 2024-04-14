ee30b8cde3b839120f089319b89a386f35e801af
Lucian Green <luciangreen@lucianacademy.com>
2024-04-15 09:25:21 +1000

    Update cat_alg_files2.pl

--- cat_alg_files2.pl
+++ cat_alg_files2.pl
@@ -1,5 +1,5 @@
 :-include('../listprologinterpreter/listprolog.pl').
-main:-catch((shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_alg_files -c cat_alg_files.pl"),
+main:-catch((shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_alg_files -c cat_alg_files1.pl"),
 shell1_s("./cat_alg_files")),Err,handle_error(Err)),halt.
 
 handle_error(_Err):-
