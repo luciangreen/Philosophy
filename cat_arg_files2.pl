c47ef84b75634489674b9dbb9e1b53b040a0351f
Lucian Green <luciangreen@lucianacademy.com>
2024-04-15 09:33:57 +1000

    Removed stack-limit for Linux

--- cat_arg_files2.pl
+++ cat_arg_files2.pl
@@ -1,5 +1,5 @@
 :-include('../listprologinterpreter/listprolog.pl').
-main:-catch((shell1_s("swipl --stack-limit=80G --goal=main --stand_alone=true -o cat_arg_files -c cat_arg_files1.pl"),
+main:-catch((shell1_s("swipl --goal=main --stand_alone=true -o cat_arg_files -c cat_arg_files1.pl"),
 shell1_s("./cat_arg_files")),Err,handle_error(Err)),halt.
 
 handle_error(_Err):-
