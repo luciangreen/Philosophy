53ece92c9f708e83d64dd4bf071b5c13a13c2b84
Lucian Green <luciangreen@lucianacademy.com>
2024-06-09 13:22:38 +1000

    Fixed s2g tests 12, 13,
    flatten_keep_brackets, check_grammar

---
+++ flatten_keep_brackets.pl
@@ -0,0 +1,10 @@
+flatten_keep_brackets(A,B) :-
+ flatten_keep_brackets2(A,[],B,true),!.
+ 
+flatten_keep_brackets2([],B,B,_) :- !.
+flatten_keep_brackets2(A,B,C,First) :-
+ (not(is_list(A))->append(B,[A],C);
+ (A=[D|E],flatten_keep_brackets2(D,B,F,true),
+ flatten_keep_brackets2(E,F,C1,false),
+ (First=true->foldr(append,[["["],C1,["]"]],C);
+ C1=C))),!.
