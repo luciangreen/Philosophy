6657b77a851819c533f12011dccc69e6ccd14428
Lucian Green <greenlucian1@gmail.com>
2026-07-24 10:08:49 +1000

    Algorithmic component of H1
    0.25 thoughts, *4 for MUH1

--- test364728.pl
+++ test364728.pl
@@ -4,6 +4,6 @@
 handle_error(_Err):-
   halt(1).
 main :-
-    catch(call_with_time_limit(2000,(N=u,M=u,texttobr2(N,u,"_1024",M,[auto,on]),texttobr(N,u,"_1024",M))), Err, handle_error(Err)), nl,
+    catch(call_with_time_limit(2000,(N=u,M=u,texttobr2(N,u,"_1176",M,[auto,on]),texttobr(N,u,"_1176",M))), Err, handle_error(Err)), nl,
     halt.
 main :- halt(1).
